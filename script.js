b2510601c9e9301dbedfcfaa154c094e05ebd39f
TagaevRenat <renattagaev0086@gmail.com>
2022-05-26 00:07:26 +0300

    fixed

--- script.js
+++ script.js
@@ -1,17 +1,49 @@
-window.onload = getImg()
-document.querySelector('#next').addEventListener('click', () => getImg(++counter))
-document.querySelector('#prev').addEventListener('click', () => getImg(--counter))
-let counter = 0
+let counter = 1
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelector('#next').addEventListener('click', () => getImg(++counter))
+    document.querySelector('#prev').addEventListener('click', () => getImg(--counter))
+    document.querySelector('#random').addEventListener('click', () => randomNumber())
+})
+window.onload = getImg(1)
 
-async function getImg() {
+async function getImg(x) {
     try {
-        let response = await fetch('https://jsonplaceholder.typicode.com/photos')
+        let response = await fetch('https://jsonplaceholder.typicode.com/photos' + '?id=' + x)
         let objects = await response.json()
-        let url = objects[counter].url
+        let url = objects[0].url
         document.querySelector('#img').src = url
-        if (counter < 0) { counter = 0 }
     }
     catch {
-        alert('Error!')
+        if (x < 1 || x > 5000) {
+            x = 1
+            getImg(x)
+        }
+        else {
+            alert('Error!')
+        }
     }
+    finally {
+        checkDisabledBtn(x)
+    }
+}
+
+function checkDisabledBtn(x) {
+    let btn1 = document.querySelector('#prev')
+    let btn2 = document.querySelector('#next')
+    if (x == 1) {
+        btn1.className = 'disabled'
+    }
+    else if (x == 5000) {
+        btn2.className = 'disabled'
+    }
+    else {
+        btn1.className = 'btn'
+        btn2.className = 'btn'
+    }
+}
+
+function randomNumber() {
+    let random = Math.floor(Math.random() * 5000)
+    ++counter
+    return getImg(random)
 }
